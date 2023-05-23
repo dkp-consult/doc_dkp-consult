ece34fec9335a1bb854460d6de942b2504fe37db
dkp-consult <96497744+dkp-consult@users.noreply.github.com>
2023-05-23 17:09:50 +0200

    (dev) : edit asto cfg - maybe fix deployement

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,11 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://doc.dkp-consult.be',
-
 	integrations: [
 		// Enable Preact to support Preact JSX components.
 		preact(),
 		// Enable React for the Algolia search component.
 		react(),
-        
 	],
-	site: `https://astro.build`,
+	site: `https://doc.dkp-consult.be`,
 });
